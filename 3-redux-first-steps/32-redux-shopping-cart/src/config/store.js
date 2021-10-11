a83f9979753a8a7b5492527af41cfde0c46144f1
Isma Navarro <ismanapa@gmail.com>
2021-10-11 17:54:49 +0200

    lesson 3-2: create shopping cart reducer and connect to component

--- 3-redux-first-steps/32-redux-shopping-cart/src/config/store.js
+++ 3-redux-first-steps/32-redux-shopping-cart/src/config/store.js
@@ -1,10 +1,12 @@
 import { createStore, combineReducers } from "redux";
 
 import productReducers from "../products/reducers";
+import shoppingCartReducers from "../shoppingcart/reducers";
 
 const store = createStore(
   combineReducers({
-    products: productReducers
+    products: productReducers,
+    shoppingcart: shoppingCartReducers,
   }),
 );
 
--- 3-redux-first-steps/32-redux-shopping-cart/src/shoppingcart/index.js
+++ 3-redux-first-steps/32-redux-shopping-cart/src/shoppingcart/index.js
@@ -1,9 +1,21 @@
+import { connect } from "react-redux";
+
 import ShoppingCart from "./components/ShoppingCart";
 
+const mapStateToProps = (state) => {
+  const products = state.products;
+  const productsOnCard = Object.entries(state.shoppingcart.products);
 
-const ShoppingCartWrapper = () => {
-  return <ShoppingCart products={[]} onCheckoutClicked={() => { }} />
-}
+  return {
+    products: productsOnCard.map(([key, value]) => {
+      const product = products[key];
+      product.quantity = value;
 
+      return product;
+    }),
+  };
+};
 
-export default ShoppingCartWrapper;
+export default connect(mapStateToProps)(
+  ShoppingCart
+);
