04ee3f1c0c88cdb08eefe80b0a459eec1028e249
Isma Navarro <ismanapa@gmail.com>
2021-10-11 18:30:40 +0200

    lesson 3-4: add redux logger

--- 3-redux-first-steps/34-redux-dev-tools/src/config/store.js
+++ 3-redux-first-steps/34-redux-dev-tools/src/config/store.js
@@ -1,11 +1,15 @@
 import { createStore, combineReducers, applyMiddleware } from "redux";
 
+import { createLogger } from "redux-logger";
 import thunk from "redux-thunk";
 
 import productReducers from "../products/reducers";
 import shoppingCartReducers from "../shoppingcart/reducers";
 
 const middlewares = [thunk];
+if (process.env.NODE_ENV !== "production") {
+  middlewares.push(createLogger());
+}
 
 const store = createStore(
   combineReducers({
