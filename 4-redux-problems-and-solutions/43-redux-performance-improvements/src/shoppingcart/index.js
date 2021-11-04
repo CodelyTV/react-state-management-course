cde40610e7b08e791b136da0afd7e0f62893cfdc
Isma Navarro <ismanapa@gmail.com>
2021-11-04 11:45:42 +0100

    lesson 4-3: redux reselect start

---
+++ 4-redux-problems-and-solutions/43-redux-performance-improvements/src/shoppingcart/index.js
@@ -0,0 +1,19 @@
+import { connect } from "react-redux";
+import { checkout } from "./actions";
+
+import ShoppingCart from "./components/ShoppingCart";
+
+const mapStateToProps = (state) => {
+  const products = state.products;
+  const productsOnCard = Object.entries(state.shoppingcart.products);
+
+  return {
+    products: productsOnCard.map(([key, value]) => {
+      return { ...products[key], quantity: value };
+    }),
+  };
+};
+
+export default connect(mapStateToProps, { onCheckoutClicked: checkout })(
+  ShoppingCart
+);
