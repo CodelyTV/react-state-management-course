53faca72226101a7d39dc5e8043c4b1e8ef150c0
Isma Navarro <ismanapa@gmail.com>
2021-11-04 12:11:46 +0100

    lesson 5-1: add logger middleware

--- 5-redux-toolkit/51-redux-toolkit-config/src/config/store.js
+++ 5-redux-toolkit/51-redux-toolkit-config/src/config/store.js
@@ -1,11 +1,14 @@
+import { configureStore } from "@reduxjs/toolkit";
+import { createLogger } from "redux-logger";
+
 import productReducers from "../products/reducers";
 import shoppingCartReducers from "../shoppingcart/reducers";
 
-import { configureStore } from "@reduxjs/toolkit";
 
 export default configureStore({
   reducer: { 
      products: productReducers,
     shoppingcart: shoppingCartReducers,
   },
+  middleware: (getDefaultMiddleware) => getDefaultMiddleware().concat(createLogger()),
 });
