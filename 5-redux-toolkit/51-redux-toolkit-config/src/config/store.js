42ad9392b456bfcc91b4e3aec6d4e213f61389ed
Isma Navarro <ismanapa@gmail.com>
2021-11-04 12:09:11 +0100

    lesson 5-1: configure store with toolkit

--- 5-redux-toolkit/51-redux-toolkit-config/src/config/store.js
+++ 5-redux-toolkit/51-redux-toolkit-config/src/config/store.js
@@ -1,23 +1,11 @@
-import { createStore, combineReducers, applyMiddleware } from "redux";
-import { composeWithDevTools } from "redux-devtools-extension";
-
-import { createLogger } from "redux-logger";
-import thunk from "redux-thunk";
-
 import productReducers from "../products/reducers";
 import shoppingCartReducers from "../shoppingcart/reducers";
 
-const middlewares = [thunk];
-if (process.env.NODE_ENV !== "production") {
-  middlewares.push(createLogger());
-}
+import { configureStore } from "@reduxjs/toolkit";
 
-const store = createStore(
-  combineReducers({
-    products: productReducers,
+export default configureStore({
+  reducer: { 
+     products: productReducers,
     shoppingcart: shoppingCartReducers,
-  }),
-  composeWithDevTools(applyMiddleware(...middlewares))
-);
-
-export default store;
+  },
+});
